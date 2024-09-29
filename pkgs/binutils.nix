aeec03591c2ef06d9b47eaf9614424bd0152434b
Artturin <Artturin@artturin.com>
2024-09-29 05:54:44 +0300

    remove unnecessary deps from binutils

--- pkgs/binutils.nix
+++ pkgs/binutils.nix
@@ -3,8 +3,6 @@
   flex,
   gmp,
   isl,
-  mpfr,
-  libmpc,
   bintools,
   fetchurl,
 }:
@@ -25,13 +23,6 @@
   buildInputs = previousAttrs.buildInputs ++ [
     gmp
     isl
-    # configure:6290: checking for isl 0.15 or later
-    # configure:6303: gcc -o conftest -g -O2      -lisl -lmpc -lmpfr -lgmp conftest.c  -lisl -lgmp >&5
-    # /nix/store/81xsp348yfgmaan9r5055mcdjfw7a8wc-binutils-2.42/bin/ld: cannot find -lmpc: No such file or directory
-    # /nix/store/81xsp348yfgmaan9r5055mcdjfw7a8wc-binutils-2.42/bin/ld: cannot find -lmpfr: No such file or directory
-    # collect2: error: ld returned 1 exit status
-    mpfr
-    libmpc
   ];
 
   #NIX_DEBUG = 7;
